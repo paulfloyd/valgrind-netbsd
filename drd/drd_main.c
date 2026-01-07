705019314d95c584341fa3d76441df9eccc7cbfa
Paul Floyd <pjfloyd@wanadoo.fr>
2026-01-07 08:21:59 +0100

    Remove DRD warning that it is not tested on this platform
    This just makes 100% of the drd regression tests fail.

--- drd/drd_main.c
+++ drd/drd_main.c
@@ -739,12 +739,6 @@ void drd__atfork_child(ThreadId tid)
 
 static void DRD_(post_clo_init)(void)
 {
-#if defined(VGO_linux) || defined(VGO_darwin) || defined(VGO_solaris) || defined(VGO_freebsd)
-   /* fine */
-#else
-   VG_(printf)("\nWARNING: DRD has not yet been tested on this operating system.\n\n");
-#  endif
-
    if (s_var_info)
    {
       VG_(needs_var_info)();
